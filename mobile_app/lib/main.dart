08a19c11545389101ed5571da60abf9f48a62d37
Derek Chen <45786074+dchen150@users.noreply.github.com>
2021-01-16 04:41:00 -0800

    trying to authenticate for google vision api

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/home_page.dart';
+import "vision.dart";
 
-void main() {
+void main() async {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: HomePage(),
-      );
+  Widget build(BuildContext context) {
+    authenticate();
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
 }
